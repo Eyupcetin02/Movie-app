38e394ff7bdf0b45d3c3d5d90999ec655fc38a7b
Eyüp Çetin <121898579+Eyupcetin02@users.noreply.github.com>
2024-08-09 14:49:01 +0300

    Update movie.js

--- server/routers/movie.js
+++ server/routers/movie.js
@@ -4,69 +4,9 @@ const {getMovie , movieFunc} = require("../movie/movieSave")
 
 
 
-/**
- * @swagger
- * /savemovie:
- *   post:
- *     summary: Yeni bir film kaydet
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               title:
- *                 type: string
- *                 example: 'Inception'
- *               director:
- *                 type: string
- *                 example: 'Christopher Nolan'
- *               year:
- *                 type: integer
- *                 example: 2010
- *     responses:
- *       200:
- *         description: Başarılı yanıt
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: 'Film başarıyla kaydedildi'
- */
+
 router.post("/savemovie", movieFunc);
 
-/**
- * @swagger
- * /getmovie:
- *   get:
- *     summary: Film bilgilerini getir
- *     responses:
- *       200:
- *         description: Başarılı yanıt
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 movies:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       title:
- *                         type: string
- *                         example: 'Inception'
- *                       director:
- *                         type: string
- *                         example: 'Christopher Nolan'
- *                       year:
- *                         type: integer
- *                         example: 2010
- */
 router.get("/getmovie", getMovie);
 
 
